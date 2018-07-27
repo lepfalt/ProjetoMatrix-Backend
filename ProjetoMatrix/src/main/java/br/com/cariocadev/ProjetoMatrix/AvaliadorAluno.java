4887c5e27cd90d2b32468170140fbe564146e825
lepfalt <lepfalt@gmail.com>
2018-07-27 19:38:52 -0300

    Teste

--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
@@ -1,25 +1,64 @@
 package br.com.cariocadev.ProjetoMatrix;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class AvaliadorAluno {
 
 	public BigDecimal getMaiorNota(BigDecimal[] notas) {
-		// Implementação o código aqui
+		if(notas != null)
+		{
+			int ma = 0;
+			for(int x=1; x<notas.length;x++)
+			{
+				if(notas[x].compareTo(notas[ma]) == 1)
+					ma = x;
+			}
+			return notas[ma];
+		}
+		
+		throw new IllegalArgumentException();
+		
+	}
+
+	public BigDecimal getMedia(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
+		
+		if(validar(nota1) && validar(nota2) && validar(nota3))
+		{
+			BigDecimal media = new BigDecimal("0.0");
+			nota2 = nota1.add(nota2);
+			nota3 = nota2.add(nota3);
+			media = nota3.divide(new BigDecimal("3.0"), 2, RoundingMode.HALF_EVEN);
+			return media;
+		}
 		
 		return null;
 	}
 
-	public BigDecimal getMedia(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
-		// Implementação o código aqui
-		return null;
-	}
-
 	public String getStatus(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
-		// Implementação o código aqui
-		return null;
+		
+		BigDecimal media = getMedia(nota1, nota2, nota3);
+		if(media != null)
+		{
+			if(media.compareTo(new BigDecimal("6.0")) == 1 ||media.compareTo(new BigDecimal("6.0")) == 0)
+				return "APROVADO";
+			else if(media.compareTo(new BigDecimal("4.0")) == 1 || media.compareTo(new BigDecimal("4.0")) == 0)
+				return "PROVA_FINAL";
+			else
+				return "REPROVADO";
+		}
+		
+		throw new IllegalArgumentException();
 	}
 	
-	
-
+	public Boolean validar(BigDecimal nota)
+	{
+		if(nota == null || nota.compareTo(new BigDecimal("0.0")) == -1 || nota.compareTo(new BigDecimal("10.0")) == 1)
+		{
+			throw new IllegalArgumentException();
+		}
+		
+		return true;
+		
+	}
 }
--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
@@ -1,22 +1,92 @@
 package br.com.cariocadev.ProjetoMatrix;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CalculadoraSalario {
 
 	public BigDecimal getSalarioLiquido(BigDecimal salarioBruto, BigDecimal percentualImpostoINSS) {
-		// Implementar o código aqui
-		return null;
+		
+		if(validar(salarioBruto) && validar(percentualImpostoINSS))
+		{
+			percentualImpostoINSS = percentualImpostoINSS.divide(new BigDecimal("100.0"), 2, RoundingMode.HALF_EVEN);
+			
+			if(salarioBruto != null && percentualImpostoINSS != null)
+			{
+				return salarioBruto.subtract(salarioBruto.multiply(percentualImpostoINSS));
+			}
+		}
+		
+		throw new IllegalArgumentException();
 	}
 
 	public BigDecimal getValorINSS(BigDecimal salarioBruto) {
-		// Implementar o código aqui
+			
+		if(validar(salarioBruto))
+		{
+			if(salarioBruto.compareTo(new BigDecimal("1693.72"))<= 0)
+			{
+				return salarioBruto.multiply(new BigDecimal("8.0")).divide(new BigDecimal("100.0"), 2, RoundingMode.HALF_UP);
+			}
+			else if(salarioBruto.compareTo(new BigDecimal("1693.73")) >= 0
+					&& salarioBruto.compareTo(new BigDecimal("2822.90")) <= 0)
+			{
+				return salarioBruto.multiply(new BigDecimal("9.0")).divide(new BigDecimal("100.0"), 2, RoundingMode.HALF_UP);
+			}
+			else if (salarioBruto.compareTo(new BigDecimal("2822.91")) >= 0)
+			{
+				return salarioBruto.multiply(new BigDecimal("11.0")).divide(new BigDecimal("100.0"), 2, RoundingMode.HALF_UP);
+			}
+		}
+		
 		return null;
 	}
 
-	public BigDecimal getValorPlanoDesaude(Integer idade) {
+	public BigDecimal getValorPlanoDeSaude(Integer idade) {
 		// TODO Auto-generated method stub
-		return null;
+		if(validar(idade))
+		{
+			if(idade >= 0 && idade <= 9)
+				return new BigDecimal("75.00");
+			else if(idade >= 10 && idade <= 19)
+				return  new BigDecimal("112.50");
+			else if(idade >= 20 && idade <= 29)
+				return  new BigDecimal("168.75");
+			else if(idade >= 30 && idade <= 39)
+				return new BigDecimal("253.13");
+			else if(idade >= 40 && idade <= 49)
+				return new BigDecimal("379.69");
+			else if(idade >= 50 && idade <= 59)
+				return new BigDecimal("569.54");
+			else
+				return new BigDecimal("854.30");
+		}
+		else
+		{
+			throw new IllegalArgumentException();
+		}
 	}
-
+	
+	public Boolean validar(BigDecimal valor)
+	{
+		if(valor == null)
+		{
+			throw new IllegalArgumentException();
+		}
+		else
+		{
+			return true;
+		}
+	}
+	
+	public Boolean validar(Integer valor)
+	{
+		if(valor == null || valor < 0)
+		{
+			return false;
+		}
+		
+		return true;
+		
+	}
 }
